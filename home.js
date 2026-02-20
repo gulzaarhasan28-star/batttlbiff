29059f9317dcb544398a5f85e0e373381f4a88bb
gulzaarhasan28-star <gulzaarhasan28@gmail.com>
2026-02-20 17:03:41 +0530

    Delete home.js

--- home.js
+++
@@ -1,8 +0,0 @@
-export function init(app) {
-    document.querySelectorAll('.game-btn').forEach(button => {
-        button.addEventListener('click', (e) => {
-            const gameName = e.target.dataset.game;
-            app.loadComponent(gameName);
-        });
-    });
-}
